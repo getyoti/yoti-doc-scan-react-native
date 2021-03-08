c511f2b2a2203d31bb6dfe18898d0f881c22acc4
asklausen <anders.skovk@gmail.com>
2021-03-08 13:29:12 +0000

    DEP-192: Lint

--- example/ios/example/AppDelegate.m
+++ example/ios/example/AppDelegate.m
@@ -25,7 +25,7 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"example"
                                             initialProperties:nil];
-  
+
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
